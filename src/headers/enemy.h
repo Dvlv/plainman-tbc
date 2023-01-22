da2454eaa9fb71cb366b3a219179059bd9d551a0
Dvlv <Dvlv@users.noreply.github.com>
2023-01-22 09:30:10 +0000

    copy over players animation attributes to enemy

--- src/headers/enemy.h
+++ src/headers/enemy.h
@@ -3,6 +3,7 @@
 #include "animation.h"
 #include "attack.h"
 #include "raylib.h"
+#include <map>
 #include <string>
 #include <vector>
 
@@ -29,6 +30,11 @@ private:
   bool *animationPlaying;
   MeleeAnimationState meleeAnimationState;
 
+  std::map<Animation, Texture2D> textures;
+  Texture2D currentTexture;
+  int animationFrameCount;
+  int currentanimationFrame;
+
 public:
   Enemy(Rectangle pos, std::string name, std::string description, int health,
         int energy, int speed, std::vector<Attack> attacks);
@@ -38,13 +44,16 @@ public:
 
   bool isDead();
   void drawHealthBar();
-  void draw();
 
   void performAttack(Attack *atk, Rectangle targetBounds,
                      bool *animationPlaying);
   Attack *selectBestAttack();
   void takeDamage(int dmg);
 
+  void updateCurrentTextureFrame();
+  void setCurrentTexture();
+
+  void draw();
   void update();
 
   virtual ~Enemy() = default;
